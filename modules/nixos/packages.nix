849ad79b1ac8694cb84608948e3491a20385a31a
Hugo <hhakem@broadinstitute.org>
2025-04-17 17:19:23 -0400

    chore: removing graphical utilities

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -4,33 +4,17 @@ with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
 
-  # Security and authentication
-  yubikey-agent
-  keepassxc
-
   # App and package management
   appimage-run
   gnumake
   cmake
   home-manager
 
-  # Media and design tools
-  vlc
-  fontconfig
-  font-manager
-
   # Productivity tools
   bc # old school calculator
-  galculator
-
-  # Audio tools
-  cava # Terminal audio visualizer
-  pavucontrol # Pulse audio controls
 
   # Testing and development tools
   direnv
-  rofi
-  rofi-calc
   postgresql
   libtool # for Emacs vterm
 
@@ -38,8 +22,6 @@ shared-packages ++ [
   flameshot
 
   # Text and terminal utilities
-  feh # Manage wallpapers
-  screenkey
   tree
   unixtools.ifconfig
   unixtools.netstat
@@ -54,17 +36,6 @@ shared-packages ++ [
   pcmanfm # File browser
   sqlite
   xdg-utils
-
-  # Other utilities
-  yad # yad-calendar is used with polybar
-  xdotool
-  google-chrome
-
-  # PDF viewer
-  zathura
-
-  # Music and entertainment
-  spotify
 
   # Editor 
   vscode # added here because use casks for darwin
