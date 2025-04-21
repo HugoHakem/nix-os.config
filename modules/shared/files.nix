748ebcf0c8420e1b328bce357d65a2fb297ccce4
Hugo <hhakem@broadinstitute.org>
2025-04-21 13:56:56 -0400

    add: emacs init command

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,3 +1,9 @@
 { pkgs, config, user, ... }:
 
-{}
+{
+    # Initializes Emacs with org-mode so we can tangle the main config
+    # Emacs 29 includes org-mode now
+    ".emacs.d/init.el" = {
+    text = builtins.readFile ../shared/config/emacs/init.el;
+  };
+}
