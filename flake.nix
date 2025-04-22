7cb5772ef28840ed2e261132cbdd1a53b1ef73ec
Hugo <hhakem@broadinstitute.org>
2025-04-22 13:19:17 -0400

    fix: modifying linux apps to make it compatible

--- flake.nix
+++ flake.nix
@@ -56,6 +56,7 @@
       };
       mkLinuxApps = system: {
         "apply" = mkApp "apply" system;
+        "build" = mkApp "build" system;
         "build-switch" = mkApp "build-switch" system;
         "install" = mkApp "install" system;
       };
