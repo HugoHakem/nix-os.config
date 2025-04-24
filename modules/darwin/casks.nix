3a502b0aa5f0d72061bff8abf509206f239705e4
Hugo <hhakem@broadinstitute.org>
2025-04-24 17:48:42 -0400

    chore(casks.nix): cleaning

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -8,18 +8,14 @@ _:
   # Communication Tools
   "zoom"
   "slack"
-  # "discord"
-  # "telegram"
   
   # Utility Tools
   "syncthing"
 
-  # Entertainment Tools
-  # "vlc"
-
   # Productivity Tools
   # "zotero" # Since I already downloaded zotero, it conflict with my version. Need to fix this.
   "notion"
+  
   # EMACS UTILITIES
   # "raycast" # used in files.nix to create a symlink to the app for EMACS
 
