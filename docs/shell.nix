2314610192ed4ed55e78ee46f659cbb18fe61f7e
Hugo <hhakem@broadinstitute.org>
2025-06-20 12:08:29 -0400

    feat: init docs

---
+++ docs/shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? let
+    lock = (builtins.fromJSON (builtins.readFile ./../flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in import nixpkgs {},
+}:
+let
+  venvDir = "./.venv";
+in
+  pkgs.mkShell {
+    name = "nixos-docs-shell";
+    buildInputs = with pkgs; [
+      uv
+    ];
+    shellHook = ''
+      # Avoid annoying prompts from keyring libraries asking for gnome-keyring, kwallet, etc. (especially useful on headless servers).
+      export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+
+      # allow uv pip to install wheels
+      unset SOURCE_DATE_EPOCH
+
+      # Triggers creation/activation of the .venv virtualenv.
+      if [ -d "${venvDir}" ]; then
+        echo "Skipping venv creation, '${venvDir}' already exists"
+      else
+        echo "Creating new venv environment in path: '${venvDir}'"
+        uv venv "${venvDir}"
+      fi
+
+      # FEEL FREE TO UPDATE WITH --extra name-of-extra-dependencies-in-pyproject.toml
+      uv sync
+      source "${venvDir}/bin/activate"
+    '';
+  }
+
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. nixos-config documentation master file, created by
+   sphinx-quickstart on Fri Jun 20 11:20:41 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+nixos-config documentation
+==========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
