ffed9db9d27e2a53fecff3fbee1b914146c051fb
HugoHakem <hakem@ebi.ac.uk>
2026-04-24 14:21:13 +0100

    feat: add nodejs for codex installation

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -4,6 +4,8 @@ with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
   dockutil
+  nodejs_24 # to install latest version of codex
+
   # EMACS UTILITIES
   # emacsPackages.pdf-tools
 ]
